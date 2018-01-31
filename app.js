f0e44ae6b8312956c421b867de46ca5008278295
AddisonNoxy <34310754+AddisonNoxy@users.noreply.github.com>
2018-01-31 17:48:54 -0600

    Updated app
    Added groundwork for camper dialogue additions.

--- app.js
+++ app.js
@@ -7,12 +7,37 @@ client.on("ready",() => {
 });
 
 var prefix = "camp!"
+var chat = "fr!"
 client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(prefix) && !message.content.startsWith(chat)) return;
   console.log('Message received!');
   if (message.author === client.user) return;
   if (message.content.startsWith(prefix + 'ping')) {
     message.channel.sendMessage('pong');
+  } else
+
+  //messages asking for clem's campster lines
+  if (message.content.startsWith(chat + 'clem')) {
+    var clemline = Math.floor(Math.random() * 4); // returns a number
+    message.channel.sendMessage(clemline);
+    message.channel.sendMessage('Clem Foote said:');
+    if (clemline === 0) {
+      message.channel.sendMessage("Speck of dust?\nOn 6/17/2003");
+    } else
+
+    if (clemline === 1) {
+      message.channel.sendMessage("From a mile away!\nOn 6/17/2003");
+    } else
+
+    if (clemline === 2) {
+      message.channel.sendMessage("Ants suck! J.T. Rocks!\nOn 6/17/2003");
+    } else
+
+    if (clemline === 3) {
+      message.channel.sendMessage("Yeah, it will be awesome!!!!!!!!\
+      If we get enough cheers together, maybe someone will start a sports\
+      team for us to cheer on.\nOn 6/9/2003");
+    }
   } else
 
   if (message.content.startsWith(prefix + 'foo')) {
