1ee744fd85d26380b3753848d719fb65306e97ff
AddisonNoxy <34310754+AddisonNoxy@users.noreply.github.com>
2018-02-01 22:40:10 -0600

    Transitioning
    Moving from extended if's to an array.

--- app.js
+++ app.js
@@ -34,11 +34,22 @@ client.on('message', message => {
     } else
 
     if (clemline === 3) {
-      message.channel.sendMessage("Yeah, it will be awesome!!!!!!!!\
-      If we get enough cheers together, maybe someone will start a sports\
-      team for us to cheer on.\nOn 6/9/2003");
+      message.channel.sendMessage("Yeah, it will be awesome!!!!!!!! If we get enough cheers together, maybe someone will start a sports team for us to cheer on.\nOn 6/9/2003");
+    } else
+
+    if (clemline === 4) {
+      message.channel.sendMessage("Ah Crystal, I guess it's just not going to work. Doesn't seem like anyone here's interested in real sports--it's all mind, mind, mind!\nBut that's okay. How would you ever appreciate the sweet taste of victory if we didn't also know the constant, comforting tonic of failure?");
     }
   } else
+
+  if (message.content.startsWith(chat + 'bobby')) {
+    var bobbyArray = [
+      'Punch face!',
+      'Punch face 2!'
+    ];
+    var bobbySelect = bobbyArray[Math.floor(Math.random()*bobbyArray.length)];
+    message.channel.sendMessage(bobbySelect);
+  }
 
   if (message.content.startsWith(prefix + 'foo')) {
     message.channel.sendMessage('bar');
